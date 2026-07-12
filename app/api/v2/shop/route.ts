7597a8a34278d7661c610a427ca2d83486974c28
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2026-07-12 12:31:07 +0330

    edit authrozation

--- app/api/v2/shop/route.ts
+++ app/api/v2/shop/route.ts
@@ -27,7 +27,7 @@ export async function GET(req: NextRequest) {
       method: "GET",
       headers: {
         "Content-type": "application/json",
-        Authorization: token,
+        'Authorization': token,
       },
     });
     const data = await upstream.json();
